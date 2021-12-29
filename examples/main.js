4d4efee7dd864640808e8a8b815691f7953cc220
egushan <git.xports.cn>
2021-12-29 11:28:37 +0800

    '添加配置项，eslint'

--- examples/main.js
+++ examples/main.js
@@ -1,11 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 
+import eui from "../packages/index";
 
-import eui from '../packages/index'
-
-console.log(eui)
-Vue.use(eui)
+console.log(eui);
+Vue.use(eui);
 
 Vue.config.productionTip = false;
 
